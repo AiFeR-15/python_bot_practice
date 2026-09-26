cc855fe4e1ddd00ae0554f2b63d222806aef8191
AiFeR-15 <solonynko2020@gmail.com>
2026-09-26 17:55:01 +0300

    Додано головне меню

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from dotenv import load_dotenv
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 load_dotenv()
 # Встав сюди свій токен у лапках
@@ -12,16 +13,30 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        # Перший внутрішній список - це верхній рядок кнопок
+        [KeyboardButton(text="🍕 Меню")],
+        
+        # Другий внутрішній список - це нижній рядок
+        [KeyboardButton(text="🛒 Мій кошик"), KeyboardButton(text="👤 Профіль")]
+    ],
+    resize_keyboard=True
+)
+
 # Ця функція зловить повідомлення, коли ти натиснеш "Start" у боті
 @dp.message(CommandStart())
 async def command_start_handler(message: types.Message):
     # message.from_user.first_name дістає твоє ім'я прямо з налаштувань Telegram
-    await message.answer(f"Привіт, {message.from_user.first_name}! Я живий, і ми починаємо.")
+    await message.answer(
+        f"Привіт, {message.from_user.first_name}! Я живий, і ми починаємо.",
+        reply_markup=main_menu)
 
 # Головна функція запуску
 async def main():
     print("Бот успішно запущено. Чекаю на повідомлення...")
     await dp.start_polling(bot)
+    
 
 if __name__ == "__main__":
     asyncio.run(main())
